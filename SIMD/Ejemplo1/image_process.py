296c031f27e048f2929fedee83c67547322a0d8a
antto <118635410+anttox@users.noreply.github.com>
2024-05-31 13:29:39 -0500

    Delete SIMD directory

--- SIMD/Ejemplo1/image_process.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-def apply_blur(image):
-    return cv2.GaussianBlur(image, (15, 15), 0)
-
-image = cv2.imread('perro.jpg')
-blurred_image = apply_blur(image)
-cv2.imwrite('blurred_image.jpg', blurred_image)
